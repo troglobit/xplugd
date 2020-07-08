f02e686b7b3a7678aab71d2c7cd36caca4a9d044
Joachim Nilsson <troglobit@gmail.com>
2020-07-08 10:55:55 +0200

    Update copyright years
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/exec.c
+++ src/exec.c
@@ -1,6 +1,6 @@
 /* Script exec handling
  *
- * Copyright (C) 2016-2019  Joachim Nilsson <troglobit@gmail.com>
+ * Copyright (C) 2016-2020  Joachim Nilsson <troglobit@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
